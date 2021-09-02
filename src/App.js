95154d0f40dab12a3675a548f2734e0887c16231
Sayan De <sayande@Sayans-MacBook-Air.local>
2021-09-02 12:00:09 +0530

    Changes v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Container from '@material-ui/core/Container'
+import DevFinder from './components/DevFinder'
+import { makeStyles } from '@material-ui/core'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const useStyles = makeStyles({
+    div: {
+        display: "grid"
+    }
+})
+
+const App = () => {
+    const classes = useStyles()
+    return (
+       <Container className={classes.div}>
+           <DevFinder />
+       </Container>
+    )
 }
-
-export default App;
+ 
+export default App
---
+++ src/components/DevFinder.js
@@ -0,0 +1,147 @@
+import { Button, Paper, TextField, Typography, Avatar, Grid} from '@material-ui/core';
+import React, { useCallback, useEffect, useState } from 'react'
+import { makeStyles } from '@material-ui/core'
+import moment from 'moment'
+
+import moon from '../icon-moon.svg'
+import company from '../icon-company.svg'
+import location from '../icon-location.svg'
+import twitter from '../icon-twitter.svg'
+import website from '../icon-website.svg'
+
+const useStyles = makeStyles({
+    div: {
+        display: "block",
+        margin: "auto",
+        width: "50%"
+    },
+    header: {
+        display: 'flex'
+    },
+    title: {
+        flexGrow: 1
+    },
+    text: {
+        flexGrow: 1
+    }
+})
+
+const DevFinder = () => {
+    const classes = useStyles()
+    const [user, setUser] = useState({})
+    const [changedName, setChangedName] = useState('')
+    const [name, setName] = useState('octocat')
+
+    const fetchData = useCallback(() => {
+        fetch(`https://api.github.com/users/${name}`)
+            .then(res => res.json())
+            .then(data => setUser(data))
+    },[name])
+
+    useEffect(() => {
+        fetchData()
+    },[fetchData])
+    return (
+        <div className={classes.div}>
+            <div className={classes.header}>
+                <Typography className={classes.title}>
+                    devfinder
+                </Typography>
+                <div className={classes.header}>
+                    <Typography>
+                        Dark
+                    </Typography>
+                    <img src={moon} alt="moon"/>
+                </div>
+            </div>
+            <Paper className={classes.header}>
+                <TextField 
+                    id="standard-basic" 
+                    label="Search Github username..." 
+                    className={classes.text} 
+                    onChange={e => {
+                        setChangedName(e.target.value)
+                    }}/>
+                <Button 
+                        variant="contained" 
+                        size="small" 
+                        color="primary"
+                        onClick={() => {
+                                setName(changedName)
+                                fetchData()
+                            }}
+                        >
+                        Search
+                </Button>
+            </Paper>
+            <Paper elevation={0}>
+                <Grid container>
+                    <Grid item md={3}>
+                        <Avatar alt="avatar" src=""/>
+                    </Grid>
+                    <Grid item md={9}>
+                        <Grid container>
+                            <Grid item md={6}>
+                                <Typography>{user.name}</Typography>
+                                <Typography>{user.login}</Typography>
+                                <br />
+                                <Typography>{user.bio ?? "This profile has no bio"}</Typography>
+                            </Grid>
+                            <Grid item md={6}>
+                                <Typography>{moment(user.created_at).format("D MMM YYYY")}</Typography>
+                            </Grid>
+                        </Grid>
+                        <Paper>
+                            <Grid container>
+                                <Grid item md={4}>
+                                    <Typography>
+                                        Repos <br />
+                                        {user.public_repos}
+                                    </Typography>
+                                </Grid>
+                                <Grid item md={4}>
+                                    <Typography>
+                                        Followers <br />
+                                        {user.followers}
+                                    </Typography>
+                                </Grid>
+                                <Grid item md={4}>
+                                    <Typography>
+                                        Following <br />
+                                        {user.following}
+                                    </Typography>
+                                </Grid>
+                            </Grid>
+                        </Paper> 
+                        <Grid container>
+                                <Grid item md={6}>
+                                    <Typography>
+                                        <img src={location} alt="location"/>
+                                        {user.location ?? "Not Available"}
+                                    </Typography>
+                                    <Typography>
+                                        <img src={website} alt="website"/> 
+                                        {user.blog ?? "Not Available"}
+                                    </Typography>    
+                                </Grid>
+                                <Grid item md={6}>
+                                    <Typography>
+                                        <img src={twitter} alt="twitter"/>  
+                                        {user.twitter_username ?? "Not Available"}
+                                    </Typography>
+                                    <Typography>
+                                        <img src={company} alt="company"/>  
+                                        {user.company ?? "Not Available"}
+                                    </Typography>  
+                                </Grid>
+                        </Grid>   
+                    </Grid>
+                </Grid>
+            </Paper>
+        </div>
+        
+        
+    );
+}
+ 
+export default DevFinder;
